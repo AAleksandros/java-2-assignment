fa0ccd39789bd361f478948c79cf3e053f3a3728
AAleksandros <aleksandrosa20@gmail.com>
2022-12-23 00:48:45 +0200

    Part 1 build.

--- jukebox-project/src/main/java/com/hua/pogsquad/JukeBox.java
+++ jukebox-project/src/main/java/com/hua/pogsquad/JukeBox.java
@@ -9,27 +9,28 @@ import gr.hua.dit.oop2.musicplayer.Player;
 import gr.hua.dit.oop2.musicplayer.PlayerException;
 import gr.hua.dit.oop2.musicplayer.PlayerFactory;
 import java.util.Random;
+import com.hua.pogsquad.m3uparser;
 
 public class JukeBox {
   public static void main(String[] args) {
     Player p = PlayerFactory.getPlayer();
    
-    if (args[0].contains(".m3u")){
-      m3uparser parser;
+    if (args[0].contains(".m3u") && (args[0].contains(".mp3") == false)){
       try {
-        parser = new m3uparser();
+        m3uparser parser = new m3uparser();
         ArrayList<String> playlist = parser.parseFile(new File(args[0]));
-        if (args[1].equals("loop")){ //plays all songs in the m3u playlist and then restarts... a loop if you would...
+        if (args.length == 2 && args[1].equals("loop")){ 
           int n = 0;
           for(;;){
             InputStream song = new FileInputStream(playlist.get(n));
+            System.out.println("Now playing:" + playlist.get(n));
             p.play(song);
             try{
               song.close();
             }catch (IOException e){
               System.err.println("wrong");
             }
-            if (n+1 == playlist.size()){
+            if (n + 1 == playlist.size()){
               n = 0;
             }else{
               n = n + 1;
@@ -37,25 +38,27 @@ public class JukeBox {
           }
         }else if (args[1].equals("random")){
           ArrayList<String> nonplayedsongs = playlist;
-          int n = playlist.size()-1;
+          int n = playlist.size();
           Random rand = new Random();
-          while (n > -1){
-            InputStream song = new FileInputStream(nonplayedsongs.get(rand.nextInt(n)));
+          while (n > 0){
+            int randnum = rand.nextInt(n);
+            InputStream song = new FileInputStream(nonplayedsongs.get(randnum));
+            System.out.println("Now playing:" + nonplayedsongs.get(randnum));
             p.play(song);
-            nonplayedsongs.remove(n);
+            nonplayedsongs.remove(randnum);
             n = n-1;
           }
-        }else{ //plays songs in their proper order
+        }else{ 
           for (int counter = 0; counter < playlist.size(); counter++){
             InputStream song = new FileInputStream(playlist.get(counter));
             p.play(song);
           }
         }
       } catch (Exception e) {
-        e.printStackTrace();
+        System.err.println("Playlist file not found.");
       }
    }else{
-    if (args[1].equals("loop")){
+    if (args[1].equals("loop") && (args[0].contains(".mp3"))){
       try {
         InputStream song = new FileInputStream(args[0]);
         for(;;){ 
@@ -68,7 +71,7 @@ public class JukeBox {
           song = new FileInputStream(args[0]);
         }
       }catch (FileNotFoundException e) {
-        System.err.println("File papakia.mp3 not found");
+        System.err.println("File" + args[0] +"Does not exist. Exiting program..." );
       } catch (PlayerException e) {
         System.err.println("Something's wrong with the player: " + e.getMessage());
       } finally {
@@ -80,7 +83,7 @@ public class JukeBox {
         InputStream song = new FileInputStream(args[0]);
         p.play(song);
       }catch (FileNotFoundException e) {
-        System.err.println("File papakia.mp3 not found");
+        System.err.println("File not found.");
       } catch (PlayerException e) {
         System.err.println("Something's wrong with the player: " + e.getMessage());
       } finally {
